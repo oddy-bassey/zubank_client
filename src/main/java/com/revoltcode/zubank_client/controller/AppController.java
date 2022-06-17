6d9115fa4c6c3da1209cba3c6f1dc6978cd3a51e
oddy-bassey <oddybassey@gmail.com>
2022-06-17 10:27:20 +0100

    added the page route contorller

---
+++ src/main/java/com/revoltcode/zubank_client/controller/AppController.java
@@ -0,0 +1,35 @@
+package com.revoltcode.zubank_client.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AppController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String auth(Model m) {
+        return "login";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getDashboard(Model m) {
+        return "index";
+    }
+
+    @RequestMapping(value = "/customers", method = RequestMethod.GET)
+    public String getCustomer(Model m) {
+        return "customers";
+    }
+
+    @RequestMapping(value = "/accounts", method = RequestMethod.GET)
+    public String getAccounts(Model m) {
+        return "accounts";
+    }
+
+    @RequestMapping(value = "/transactions", method = RequestMethod.GET)
+    public String getTransactions(Model m) {
+        return "transactions";
+    }
+}
