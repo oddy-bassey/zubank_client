220c172c4ab61bf4480a7aeb1c4b6074c80f365b
oddy-bassey <oddybassey@gmail.com>
2022-06-17 07:34:55 +0100

    initial project setup

---
+++ src/main/java/com/revoltcode/zubank_client/ZubankClientApplication.java
@@ -0,0 +1,13 @@
+package com.revoltcode.zubank_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZubankClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZubankClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revoltcode/zubank_client/ZubankClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revoltcode.zubank_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZubankClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
